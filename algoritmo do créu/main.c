3c06d75bc3504d74e916efc753e78ec7e08d1f86
lcarrafabr <lcarrafa.br@gmail.com>
2019-02-15 19:48:29 -0200

    primeiro comit

---
+++ algoritmo do créu/main.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Algoritmo do Créu - Algoritmo escrito por Luciano Carrafa Benfica
+int main(int argc, char *argv[])
+{int resp, repetir;
+
+
+printf("\n\n\n\n\n");
+printf("Chaveca a gata: ");
+printf("\n\n\n\n\n\n\n\n\n\n");
+printf("E das que da?\n");
+printf("Digite (1) p/ NAO - ou (2) p/ SIM: ");
+scanf("%d",&resp);
+if(resp==1){
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Entao manda tomar no cu \n\n");
+            printf("Bate uma bronha (5 contra 1) \n\n");
+            printf("Lave as maos \n\n");
+            printf("a noite acabou \n\n");
+            printf("Se fudeu\n\n");
+            printf("Vai dormir\n\n\n\n\n\n\n");
+                     }
+if(resp==2){
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Chama para sair\n\n\n\n\n\n");
+            printf("Aceitou? \n");
+            printf("Digite (1) p/ NAO - ou (2) p/ SIM: ");
+            scanf("%d",&resp);
+if(resp==1){
+            printf("\n\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Chaveca mais\n\n");
+            printf("Chama pra sair\n\n");
+            printf("Aceitou? \n");
+            printf("Digite (1) p/ NAO - ou (2) p/ SIM: ");
+            scanf("%d",&resp);
+if(resp==1){
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Entao manda tomar no cu \n\n");
+            printf("Bate uma bronha (5 contra 1) \n\n");
+            printf("Lave as maos \n\n");
+            printf("a noite acabou \n\n");
+            printf("Se fudeu\n\n");
+            printf("Vai dormir\n\n\n");
+            }}
+if(resp==2){
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Beija na Boca\n\n");
+            printf("Pega nos peitinhos\n\n");
+            printf("Reclamou?\n\n\n");
+            printf("Digite (1) p/ SIM - ou (2) p/ NAO: ");
+            scanf("%d",&resp);
+if(resp==1){
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Diz que ela e tudo na sua vida\n\n");
+            printf("Pega nos peitinhos\n\n");
+            printf("Reclamou?\n\n");
+            printf("Digite (1) p/ SIM - ou (2) p/ NAO: ");
+            scanf("%d",&resp);
+if(resp==1){
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Entao manda tomar no cu \n\n");
+            printf("Bate uma bronha (5 contra 1) \n\n");
+            printf("Lave as maos \n\n");
+            printf("a noite acabou \n\n");
+            printf("Se fudeu\n\n");
+            printf("Vai dormir\n\n\n\n");
+                     }}
+if(resp==2){
+            repetir=2;
+            while(repetir==2){
+                              printf("\n\n\n\n");
+                              printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+                              printf("MOVE YOUR HANDS TO XOXOTA\n\n\n");
+                              printf("Ta molhadinha? \n\n");
+                              printf("Digite (1) p/SIM - ou (2) p/ NAO: ");
+                              scanf("%d",&repetir);}
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Ta de chico?\n");
+            printf("Digite (1) p/ Sim - ou (2) p/ NAO: ");
+            scanf("%d",&resp);{
+if(resp==1){
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Calma, ve se dapra bater uma bola atraz do gol\n\n\n");
+            printf("Digite (1) p/ NAO - ou (2) p/ SIM: ");
+            scanf("%d",&resp);}
+            if(resp==1){
+                        printf("\n\n\n\n");
+                        printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+                        printf("Entao manda tomar no cu \n\n");
+                        printf("Bate uma bronha (5 contra 1) \n\n");
+                        printf("Lave as maos \n\n");
+                        printf("a noite acabou \n\n");
+                        printf("Se fudeu\n\n");
+                        printf("Vai dormir\n\n\n");}{
+if(resp==2
+){
+            printf("\n\n\n\n");
+            printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n");
+            printf("Coloque a camisinha\n\n\n\n");
+            repetir=2;
+            while(repetir==2){
+                              printf("Tira e Bota N vezes\n\n");
+                              printf("Gozou?\n\n");
+                              printf("Digite (1) p/ SIM - ou (2) p/ NAO \n\n");
+                              scanf("%d",&repetir);}}
+                              printf("\n\n\n\n");
+                              printf("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n\n\n\n\n");
+                              printf("Vai dormir\n\n\n\n\n\n\n");
+}
+ }
+  }
+   }
+    }
+  system("PAUSE");	
+  return 0;
+}
